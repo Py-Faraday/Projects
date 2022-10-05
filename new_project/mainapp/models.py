6b999a6a72550ac959b0a9879684b5a375713e4c
saparova <saparovaaibarchyn777.ru>
2022-10-05 12:42:57 +0600

    new_project

--- new_project/mainapp/models.py
+++ new_project/mainapp/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 
@@ -8,16 +9,30 @@ class Product(models.Model):
     price = models.IntegerField()
     articul = models.CharField(max_length = 127)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Cart(models.Model):
-    user_name = models.CharField(max_length = 127, verbose_name = 'Имя')
-    created_at  = models.DateTimeField(auto_now_add=True, verbose_name = 'Дата создания')
-    total_price = models.IntegerField(verbose_name = ' Баланс в Карте')
-    delivery_address = models.CharField(max_length = 127, verbose_name = 'Адрес')
+    user_name = models.CharField(max_length = 127)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField(default=0)
+    delivery_address = models.CharField(max_length = 127)
 
 
 class CartProduct(models.Model):
-    product = models.ForeignKey(Product , on_delete = models.CASCADE)
-    cart = models.ForeignKey(Cart , on_delete = models.CASCADE)
-    total_price = models.IntegerField()
+    product = models.ForeignKey(Product , on_delete = models.CASCADE, related_name = 'cart_products')
+    cart = models.ForeignKey(Cart , on_delete = models.CASCADE,related_name = 'cart_products')
+    total_price = models.IntegerField(default=0)
     amount = models.IntegerField()
+
+
+    @property
+    def sum_amount(self):
+        return self.product.price * self.amount
+
+    def __str__(self):
+        return self.product.name
+        
+
